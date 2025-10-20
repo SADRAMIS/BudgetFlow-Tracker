cd238a6ce2652465f13a0819ee4ae12b2eca6bd4
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-10-20 18:44:44 +0300

    Init commit

---
+++ src/main/java/com/example/budgetflow/BudgetFlowTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgetflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetFlowTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetFlowTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/budgetflow/BudgetFlowTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budgetflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetFlowTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
