cce457e8a123c3e564b7ff072a6fefc350e3608d
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-10-24 14:39:00 +0300

    add Transaction

---
+++ src/main/java/com/example/budgetflow/entity/Transaction.java
@@ -0,0 +1,41 @@
+package com.example.budgetflow.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "transactions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",nullable = false)
+    private Category category;
+
+}
