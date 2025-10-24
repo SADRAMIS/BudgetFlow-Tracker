34df75dbedb7e1ed7d18004d1aa34297111e696c
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-10-24 14:25:34 +0300

    add Category

---
+++ src/main/java/com/example/budgetflow/entity/Category.java
@@ -0,0 +1,33 @@
+package com.example.budgetflow.entity;
+
+import jakarta.persistence.*;
+import jakarta.transaction.Transaction;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String type;
+
+    private String icon; // иконка категории
+    private String color; // цвет категории для UI
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Transaction> transactions;
+}
